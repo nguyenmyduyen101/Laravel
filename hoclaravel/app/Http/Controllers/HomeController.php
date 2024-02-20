e3ffd8695153a63ae2bf01ae4ca434ee47ea9286
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-02-20 09:31:35 +0700

    Laravel-Bai11

--- hoclaravel/app/Http/Controllers/HomeController.php
+++ hoclaravel/app/Http/Controllers/HomeController.php
@@ -7,35 +7,20 @@ use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
-    //action index
-    public function index()
-    {
-        $title = " Học lập trình web tại Unicode";
-        $content = "Học lập trình Laravel 8.x tại Unicode";
-        
-        //compact('title','content')
-        return view('home')->with(['title'=> $title, 'content'=>$content]); //load view home.php
+    public $data = [];
+    public function index(){
+        $this->data['Welcome'] = 'Học lập trình Laravel tại <b>Unicode</b>';
+        $this->data['content'] = '<h3>Chương 1: Nhập môn Laravel</h3>
+        <p>Kiến thức 1</p>
+        <p>Kiến thức 2</p>
+        <p>Kiến thức 3</p>';
 
-        //return View::make('home')->with(['title'=> $title, 'content'=>$content]);
+        $this->data['index'] =1;
 
-        // $contentView = view('home')->render();
-        // //$contentView = $contentView->render();
-        // dd($contentView);
-        //return $contentView;
-    }
+        $this->data['dataArr'] = [];
 
-    //action getNews
-    public function getNews()
-    {
-        return 'Danh sách tin tức';
-    }
+        $this->data['number'] = 9;
 
-    public function getCategories($id)
-    {
-        return 'Chuyên mục' . $id;
-    }
-
-    public function getProductDetail($id){
-        return view('clients.products.detail',compact('id'));
+        return view('home',$this->data);
     }
 }
