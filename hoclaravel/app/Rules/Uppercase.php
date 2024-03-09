300ab44280206a9ac20037f6e894c48d3c60c274
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-03-09 09:47:37 +0700

    Laravel-Bai25

---
+++ hoclaravel/app/Rules/Uppercase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Uppercase implements ValidationRule
+{
+    private $attribute = null;
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        //
+    }
+
+    public function passes($attribute,$value){
+        //
+        $this->attribute = $attribute;
+        if ($value=mb_strtoupper($value,'UTF-8')){
+            return true;
+
+        }
+        return false;
+
+    }
+
+    public function message(){
+        //return 'Trường :atribute không hợp lệ';
+
+        $customMessage = 'validation.custom.'.($this->attribute).'.uppercase';
+
+        if (trans($customMessage)!=$customMessage){
+            return trans($customMessage);
+        }
+        return trans('validation.uppercase');
+    }
+
+}
