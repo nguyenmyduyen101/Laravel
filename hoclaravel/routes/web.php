5616a35453a04ffa52427f7d23cf3e4c23884434
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-03-16 10:40:42 +0700

    Laravel-Bai29

--- hoclaravel/routes/web.php
+++ hoclaravel/routes/web.php
@@ -55,6 +55,12 @@ Route::get('download-doc',[HomeController::class,'downloadDoc'])->name('download
 
 
 //Người dùng
-Route::prefix('users')->group(function(){
-  Route::get('/',[UsersController::class,'index']);
+Route::prefix('users')->name('users.')->group(function(){
+  Route::get('/',[UsersController::class,'index'])->name('index');
+
+  Route::get('/add',[UsersController::class,'add'])->name('add');
+
+  Route::post('/add',[UsersController::class,'postAdd'])->name('post-add');
+
+
 });
