0e016df32fa07f73b53c30d4a2da1b8eca0c6067
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-03-28 11:23:04 +0700

    Laravel-Bai43

--- hoclaravel/app/Http/Controllers/UsersController.php
+++ hoclaravel/app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 
+
 // use Illuminate\Support\Facades\DB;
 
 use App\Models\Users;
--- hoclaravel/app/Models/Users.php
+++ hoclaravel/app/Models/Users.php
@@ -18,8 +18,8 @@ class Users extends Model
        //DB::enableQueryLog();
        $users = DB::table($this->table)
        ->select('users.*','groups.name as group_name')
-       ->join('groups','users.group_id','=','groups.id');
-
+       ->join('groups','users.group_id','=','groups.id')
+        ->where('trash',0);
        $orderBy = 'users.create_at';
        $orderType = 'desc';
 
@@ -78,7 +78,8 @@ class Users extends Model
     }
 
     public function deleteUser($id){
-        return DB::delete("DELETE FROM $this->table WHERE id=?",[$id]);
+        //return DB::delete("DELETE FROM $this->table WHERE id=?",[$id]);
+        return DB::table($this->table)->where('id',$id)->delete();
     }
 
     public function statementUser($sql){
